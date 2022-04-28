38e2ab5a19aa12c66ef4f4c6557d0d945bd15f67
coderbennett <jbennett@csumb.edu>
2022-04-27 19:54:18 -0700

    add notes.js with path for posting notes

---
+++ routes/notes.js
@@ -0,0 +1,38 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+notes.get('/', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (error, data) => {
+        res.json(JSON.parse(data));
+    });
+});
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            noteId: uuidv4(),
+        };
+
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const currentNotes = JSON.parse(data);
+                currentNotes.push(newNote);
+                fs.writeFile('./db/db.json', JSON.stringify(currentNotes), (err) => 
+                    err ? console.error(err) : console.log('updated notes file successfully')
+                );
+            }
+        })
+        res.json('Note added to db.json');
+    } else {
+        res.error('Error!! Note was not added.');
+    }
+});
+
+module.exports = notes;
