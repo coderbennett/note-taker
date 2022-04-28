56d2dfc194ea04a3c5604ce5cd85a800613ed10d
coderbennett <jbennett@csumb.edu>
2022-04-27 19:25:23 -0700

    add api path to add notes router through

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3000;
 
@@ -7,17 +8,22 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({}));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
-//establish the root route to be the index.html
+//establish the root get route to be the index.html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-//establish the /notes route to be the notes.html
+//establish the /notes get route to be the notes.html
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () => 
